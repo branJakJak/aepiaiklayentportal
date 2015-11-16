89aa15b44c0fbf954dead36a5e4e772af7ba432c
kebyang <kelvo_link@yahoo.com>
2015-11-17 01:28:21 +0800

    asdasd

--- protected/config/main.php
+++ protected/config/main.php
@@ -117,8 +117,8 @@ return array(
             'class' => 'CLogRouter',
             'routes' => array(
                 array(
-                    'class' => 'CWebLogRoute',
-                    // 'class' => 'CFileLogRoute',
+                    // 'class' => 'CWebLogRoute',
+                    'class' => 'CFileLogRoute',
                     'levels' => 'error, warning',
                 ),
             // uncomment the following to show log messages on web pages
