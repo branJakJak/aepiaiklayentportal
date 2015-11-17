cb6867ff810bec49dd26a43dc74a26ad8f0d4324
kebyang <kelvo_link@yahoo.com>
2015-11-17 22:25:03 +0800

    updated grid cols

--- themes/abound/views/site/index.php
+++ themes/abound/views/site/index.php
@@ -118,7 +118,7 @@ Yii::app()->clientScript->registerScript($updateEvery60, $updateEvery60, CClient
                     'value'=>'$data["calls"]',
                 ),
                 array(
-                    'header'=>'5CXFER',
+                    'header'=>'Diable Leads',
                     'type'=>'raw',
                     'value'=>'$data["leads"]',
                 ),
