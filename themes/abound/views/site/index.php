29f04b9b6e7159002a97af13a4f61007a6a4e1fa
kebyang <kelvo_link@yahoo.com>
2015-11-17 21:20:08 +0800

    BE9rm6DO0BpyUfhsSfbD

--- themes/abound/views/site/index.php
+++ themes/abound/views/site/index.php
@@ -118,7 +118,7 @@ Yii::app()->clientScript->registerScript($updateEvery60, $updateEvery60, CClient
                     'value'=>'$data["calls"]',
                 ),
                 array(
-                    'header'=>'Dialable Leads',
+                    'header'=>'5CXFER',
                     'type'=>'raw',
                     'value'=>'$data["leads"]',
                 ),
