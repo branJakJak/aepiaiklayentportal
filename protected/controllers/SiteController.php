fe0050a4f17a9b6425a5d34b9abb1e3e7ed7be26
kebyang <kelvo_link@yahoo.com>
2015-11-17 23:24:35 +0800

    balance zero

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -47,7 +47,7 @@ class SiteController extends Controller
 			$tempContainer = $clientVb[$key];
 			$tempContainer['id'] = uniqid();
 			$tempContainer['total'] = (  ( doubleval($value['seconds']) ) / 60  ) * doubleval($value['ppminc']);
-			$tempContainer['balance'] = doubleval($value['balance']) + 300;
+			$tempContainer['balance'] = doubleval($value['balance']) + 0;
 			$tempContainer['balance'] -= doubleval($tempContainer['total']);
 			$tempContainer['balance'] = '£ '.$tempContainer['balance'];
 			$tempContainer['cxfer']  = $_5CXFER['generated'];
