689fb7ee35a1e500cffa5e9cca566b5cbb8867e2
kebyang <kelvo_link@yahoo.com>
2016-02-03 02:34:15 +0800

    bal;alce'

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -57,7 +57,7 @@ class SiteController extends Controller
 			$tempContainer = $clientVb[$key];
 			$tempContainer['id'] = uniqid();
 			$tempContainer['total'] = (  ( doubleval($value['seconds']) ) / 60  ) * doubleval($value['ppminc']);
-			$tempContainer['balance'] = doubleval($value['balance']) + $updatedInitBalance;
+			// $tempContainer['balance'] = doubleval($value['balance']) + $updatedInitBalance;
 			$tempContainer['balance'] -= doubleval($tempContainer['total']);
 			$tempContainer['balance'] = '£ '.$tempContainer['balance'];
 
