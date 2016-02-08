4644157053ddfb98e89a43af96610caedfd7c445
kek <top_kek@kek.com>
2016-02-08 20:48:24 +0800

    in seconds

--- themes/abound/views/site/index.php
+++ themes/abound/views/site/index.php
@@ -208,12 +208,11 @@ Yii::app()->clientScript->registerScript($updateEvery60, $updateEvery60, CClient
                     'type'=>'raw',
                     'value'=>'$data["seconds"]',
                 ),
-                
-                // array(
-                //     'header' => 'Seconds',
-                //     'type' => 'raw',
-                //     'value' => '$data["raw_seconds"]',
-                // ),
+                array(
+                    'header' => 'Total in Seconds',
+                    'type' => 'raw',
+                    'value' => '$data["raw_seconds"]',
+                ),
                 // 'raw_seconds',
                 // 'calls',
                 // 'generated',
